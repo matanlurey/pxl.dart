8c7a8e28406be713a4bb26c870241ff738552889
Matan Lurey <matan@lurey.org>
2024-08-30 16:55:13 -0700

    Now that we have tests, start to rewrite the impl.

--- lib/src/codec/netpbm.dart
+++ lib/src/codec/netpbm.dart
@@ -136,45 +136,7 @@ final class NetpbmAsciiEncoder extends NetpbmEncoder<String> {
     Buffer<int> input, {
     Iterable<String> comments = const [],
   }) {
-    final format = _getOrInferFormat(input);
-    final output = StringBuffer('${format._ascii}\n');
-    output.writeAll(comments.map((c) => '# $c\n'));
-    output.writeln('${input.width} ${input.height}');
-
-    final String Function(int) describe;
-    switch (format) {
-      case NetpbmFormat.bitmap:
-        describe = (pixel) => pixel == input.format.zero ? '0' : '1';
-      case NetpbmFormat.graymap:
-        output.writeln(gray8.maxGray);
-        describe = (pixel) {
-          final gray = gray8.convert(pixel, from: input.format);
-          return gray.toString().padLeft(3);
-        };
-      case NetpbmFormat.pixmap:
-        output.writeln(rgb888.maxRed);
-        describe = (pixel) {
-          final rgb = rgb888.convert(pixel, from: input.format);
-          final red = rgb888.getRed(rgb).toString().padLeft(3);
-          final green = rgb888.getGreen(rgb).toString().padLeft(3);
-          final blue = rgb888.getBlue(rgb).toString().padLeft(3);
-          return '$red $green $blue';
-        };
-    }
-
-    for (var y = 0; y < input.height; y++) {
-      for (var x = 0; x < input.width; x++) {
-        final pixel = input.getUnsafe(Pos(x, y));
-        output.write(describe(pixel));
-        if (x < input.width - 1) {
-          output.write(' ');
-        }
-      }
-      if (y < input.height - 1) {
-        output.writeln();
-      }
-    }
-    return output.toString();
+    throw UnimplementedError();
   }
 }
 
@@ -192,44 +154,7 @@ final class NetpbmBinaryEncoder extends NetpbmEncoder<Uint8List> {
     Buffer<int> input, {
     Iterable<String> comments = const [],
   }) {
-    final format = _getOrInferFormat(input);
-    final output = StringBuffer('${format._binary}\n');
-    output.writeAll(comments.map((c) => '# $c\n'));
-    output.writeln('${input.width} ${input.height}');
-
-    final buffer = BytesBuilder(copy: false);
-    final void Function(int) write;
-    switch (format) {
-      case NetpbmFormat.bitmap:
-        output.write('1\n');
-        write = (pixel) {
-          buffer.addByte(pixel == input.format.zero ? 0 : 1);
-        };
-      case NetpbmFormat.graymap:
-        output.write('${gray8.maxGray}\n');
-        write = (pixel) {
-          final gray = gray8.convert(pixel, from: input.format);
-          buffer.addByte(gray);
-        };
-      case NetpbmFormat.pixmap:
-        output.write('${rgb888.maxRed}\n');
-        write = (pixel) {
-          final rgb = rgb888.convert(pixel, from: input.format);
-          buffer.addByte(rgb888.getRed(rgb));
-          buffer.addByte(rgb888.getGreen(rgb));
-          buffer.addByte(rgb888.getBlue(rgb));
-        };
-    }
-    buffer.add(utf8.encode(output.toString()));
-
-    for (var y = 0; y < input.height; y++) {
-      for (var x = 0; x < input.width; x++) {
-        final pixel = input.getUnsafe(Pos(x, y));
-        write(pixel);
-      }
-    }
-
-    return buffer.toBytes();
+    throw UnimplementedError();
   }
 }
 
@@ -246,6 +171,26 @@ abstract final class NetpbmDecoder<T> extends Converter<T, Buffer<int>> {
   /// If omitted, the pixel format defauls to a fully opaque [abgr8888].
   /// {@endtemplate}
   final PixelFormat<int, void> format;
+
+  /// Parses the header information from the Netpbm image.
+  ///
+  /// If the header is invalid or missing, a [FormatException] is thrown.
+  NetpbmHeader parseHeader(T input) {
+    final (header, error, offset) = _parseHeader(input);
+    if (header == null) {
+      throw FormatException(error, input);
+    }
+    return header;
+  }
+
+  /// Parses the header information from the Netpbm image.
+  ///
+  /// If the header is invalid or missing, `null` is returned.
+  NetpbmHeader? tryParseHeader(T input) {
+    return _parseHeader(input).$1;
+  }
+
+  (NetpbmHeader? head, String error, int start) _parseHeader(T input);
 }
 
 /// Decodes a portable pixmap (Netpbm) image format using ASCII to pixel data.
@@ -256,120 +201,13 @@ final class NetpbmAsciiDecoder extends NetpbmDecoder<String> {
   const NetpbmAsciiDecoder({super.format});
 
   @override
+  (NetpbmHeader? head, String error, int start) _parseHeader(String input) {
+    throw UnimplementedError();
+  }
+
+  @override
   Buffer<int> convert(String input) {
-    final NetpbmFormat format;
-    final lines = const LineSplitter()
-        .convert(input)
-        .map((l) => l.trim())
-        .where((l) => l.isNotEmpty && !l.startsWith('#'))
-        .iterator;
-
-    if (!lines.moveNext()) {
-      throw FormatException('Invalid Netpbm image: missing format');
-    }
-    final header = lines.current.trim();
-    switch (header) {
-      case 'P1':
-        format = NetpbmFormat.bitmap;
-      case 'P2':
-        format = NetpbmFormat.graymap;
-      case 'P3':
-        format = NetpbmFormat.pixmap;
-      default:
-        throw FormatException(
-          'Invalid Netpbm image: unsupported format',
-          header,
-        );
-    }
-
-    if (!lines.moveNext()) {
-      throw FormatException('Invalid Netpbm image: missing dimensions');
-    }
-
-    final dimensions = lines.current.split(' ');
-    if (dimensions.length != 2) {
-      throw FormatException(
-        'Invalid Netpbm image: invalid dimensions',
-        dimensions,
-      );
-    }
-
-    final width = int.tryParse(dimensions[0]);
-    final height = int.tryParse(dimensions[1]);
-    if (width == null || height == null || width < 1 || height < 1) {
-      throw FormatException(
-        'Invalid Netpbm image: invalid dimensions',
-        dimensions,
-      );
-    }
-
-    final buffer = IntPixels(width, height, format: this.format);
-
-    // Skip the optional maximum value if present.
-    if (format != NetpbmFormat.bitmap && lines.moveNext()) {
-      final max = int.tryParse(lines.current);
-      if (max == null || max < 1) {
-        throw FormatException(
-          'Invalid Netpbm image: invalid maximum value',
-          lines.current,
-        );
-      }
-    }
-
-    // Keep parsing pixels until the end of the image, skipping whitespace.
-    var offset = 0;
-    while (lines.moveNext()) {
-      final line = lines.current;
-      final values = line
-          .trim()
-          .replaceAll(RegExp(r'\s+'), ' ')
-          .split(' ')
-          .map(int.tryParse)
-          .toList();
-      if (values.any((v) => v == null)) {
-        throw FormatException(
-          'Invalid Netpbm image: invalid pixel value(s)',
-          line,
-        );
-      }
-
-      final Iterable<int> pixels;
-      switch (format) {
-        case NetpbmFormat.bitmap:
-          pixels = values.map(
-            (v) => v == 0 ? this.format.zero : this.format.max,
-          );
-        case NetpbmFormat.graymap:
-          pixels = values.map((v) {
-            final gray = gray8.create(gray: v);
-            return this.format.convert(gray, from: gray8);
-          });
-        case NetpbmFormat.pixmap:
-          if (values.length % 3 != 0) {
-            throw FormatException(
-              'Invalid Netpbm image: invalid pixel count',
-              line,
-            );
-          }
-          pixels = Iterable.generate(
-            values.length ~/ 3,
-            (i) {
-              final offset = i * 3;
-              final rgb = abgr8888.create(
-                red: values[offset],
-                green: values[offset + 1],
-                blue: values[offset + 2],
-              );
-              return this.format.convert(rgb, from: abgr8888);
-            },
-          );
-      }
-
-      buffer.data.setAll(offset, pixels);
-      offset += pixels.length;
-    }
-
-    return buffer;
+    throw UnimplementedError();
   }
 }
 
@@ -383,5 +221,120 @@ final class NetpbmBinaryDecoder extends NetpbmDecoder<Uint8List> {
   @override
   Buffer<int> convert(Uint8List input) {
     throw UnimplementedError();
+  }
+
+  static bool _isLinebreak(int byte) => byte == 0x0A;
+  static bool _isWhitespace(int byte) {
+    return byte == 0x20 || byte >= 0x09 && byte <= 0x0D;
+  }
+
+  (String, int) _readUntil(
+    Uint8List input,
+    int start, [
+    bool Function(int) test = _isLinebreak,
+  ]) {
+    final result = StringBuffer();
+    for (; start < input.length; start++) {
+      final byte = input[start];
+      if (test(byte)) {
+        start++;
+        break;
+      }
+      result.writeCharCode(byte);
+    }
+    return (result.toString(), start);
+  }
+
+  int _readComments(Uint8List input, List<String> output, int start) {
+    while (input[start] == 0x23) {
+      final (comment, next) = _readUntil(input, start + 1);
+
+      // Check for and throw if EOF.
+      if (next == input.length) {
+        throw FormatException('Unexpected EOF', input, start);
+      }
+
+      output.add(comment);
+      start = next;
+    }
+    return start;
+  }
+
+  @override
+  (NetpbmHeader? head, String error, int start) _parseHeader(Uint8List input) {
+    throw UnimplementedError();
+  }
+}
+
+/// Parsed header information from a Netpbm image.
+@immutable
+final class NetpbmHeader {
+  /// Creates a new Netpbm header with the given values.
+  NetpbmHeader({
+    required this.format,
+    required this.width,
+    required this.height,
+    this.max,
+    Iterable<String> comments = const [],
+  }) : comments = List.unmodifiable(comments);
+
+  /// The format of the Netpbm image.
+  final NetpbmFormat format;
+
+  /// The width of the image.
+  final int width;
+
+  /// The height of the image.
+  final int height;
+
+  /// The maximum value of a pixel in the image.
+  final int? max;
+
+  /// Comments associated with the image.
+  final List<String> comments;
+
+  @override
+  bool operator ==(Object other) {
+    if (other is! NetpbmHeader) {
+      return false;
+    }
+    if (format != other.format) {
+      return false;
+    }
+    if (width != other.width) {
+      return false;
+    }
+    if (height != other.height) {
+      return false;
+    }
+    if (max != other.max) {
+      return false;
+    }
+    if (comments.length != other.comments.length) {
+      return false;
+    }
+    for (var i = 0; i < comments.length; i++) {
+      if (comments[i] != other.comments[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  @override
+  int get hashCode {
+    return Object.hash(format, width, height, max, Object.hashAll(comments));
+  }
+
+  @override
+  String toString() {
+    final buffer = StringBuffer('NetpbmHeader(\n');
+    buffer.writeln('  format: $format,');
+    buffer.writeln('  width: $width,');
+    buffer.writeln('  height: $height,');
+    buffer.writeln('  max: $max,');
+    buffer.writeln('  comments: $comments,');
+    buffer.write(')');
+    return buffer.toString();
   }
 }
---
+++ test/codec/netpbm_binary_test.dart
@@ -0,0 +1,139 @@
+import 'dart:convert';
+import 'dart:typed_data';
+
+import 'package:pxl/pxl.dart';
+
+import '../src/prelude.dart';
+
+void main() {
+  test('width must be at least 1', () {
+    check(
+      () => netpbmBinaryEncoder.convert(_ZeroWidthBuffer()),
+    ).throws<ArgumentError>();
+  });
+
+  test('height must be at least 1', () {
+    check(
+      () => netpbmBinaryEncoder.convert(_ZeroHeightBuffer()),
+    ).throws<ArgumentError>();
+  });
+
+  test('explicit bitmap', () {
+    final pixels = IntPixels(3, 2);
+    pixels.set(Pos(0, 0), abgr8888.white);
+    pixels.set(Pos(1, 0), abgr8888.white);
+    pixels.set(Pos(2, 0), abgr8888.white);
+
+    final encoded = const NetpbmBinaryEncoder(
+      format: NetpbmFormat.bitmap,
+    ).convert(pixels);
+    check(encoded).deepEquals(
+      Uint8List.fromList([
+        ...utf8.encode('P4\n3 2\n'),
+        1, 1, 1, //
+        0, 0, 0, //
+      ]),
+    );
+
+    // Try decoding the encoded string.
+    final decoded = netpbmBinaryDecoder.convert(encoded);
+    check(decoded.format).equals(pixels.format);
+    check(decoded.compare(pixels).difference).equals(0.0);
+  });
+
+  test('implicit bitmap', () {
+    final monochrome = IndexedFormat.bits8(
+      [
+        abgr8888.black,
+        abgr8888.white,
+      ],
+      format: abgr8888,
+    );
+    final pixels = IntPixels(3, 2, format: monochrome);
+    pixels.set(Pos(0, 0), 1);
+    pixels.set(Pos(1, 0), 1);
+    pixels.set(Pos(2, 0), 1);
+
+    final encoded = netpbmBinaryEncoder.convert(pixels);
+    check(encoded).deepEquals(
+      Uint8List.fromList([
+        ...utf8.encode('P4\n3 2\n'),
+        1, 1, 1, //
+        0, 0, 0, //
+      ]),
+    );
+
+    // Try decoding the encoded string.
+    final decoded = NetpbmBinaryDecoder(format: monochrome).convert(encoded);
+    check(decoded.compare(pixels).difference).equals(0.0);
+  });
+
+  test('implicit gray8', () {
+    // Use multiple shades of gray to test the graymap format.
+    final pixels = IntPixels(3, 1, format: gray8);
+    pixels.set(Pos(0, 0), gray8.create(gray: 0));
+    pixels.set(Pos(1, 0), gray8.create(gray: 128));
+    pixels.set(Pos(2, 0), gray8.create(gray: 255));
+
+    final encoded = const NetpbmBinaryEncoder(
+      format: NetpbmFormat.graymap,
+    ).convert(pixels);
+
+    check(encoded).deepEquals(
+      Uint8List.fromList([
+        ...utf8.encode('P5\n3 1\n255\n'),
+        0, 128, 255, //
+      ]),
+    );
+
+    // Try decoding the encoded string.
+    final decoded = NetpbmBinaryDecoder(format: gray8).convert(encoded);
+    check(decoded.format).equals(pixels.format);
+    check(decoded.compare(pixels).difference).equals(0.0);
+  });
+
+  test('implicit pixmap', () {
+    final pixels = IntPixels(3, 1);
+    pixels.set(Pos(0, 0), abgr8888.red);
+    pixels.set(Pos(1, 0), abgr8888.green);
+    pixels.set(Pos(2, 0), abgr8888.blue);
+
+    final encoded = const NetpbmBinaryEncoder(
+      format: NetpbmFormat.pixmap,
+    ).convert(pixels);
+
+    check(encoded).deepEquals(
+      Uint8List.fromList([
+        ...utf8.encode('P6\n3 1\n255\n'),
+        255, 0, 0, 0, 255, 0, 0, 0, 255, //
+      ]),
+    );
+
+    // Try decoding the encoded string.
+    final decoded = netpbmBinaryDecoder.convert(encoded);
+    check(decoded.format).equals(pixels.format);
+    check(decoded.compare(pixels).difference).equals(0.0);
+  });
+}
+
+final class _ZeroWidthBuffer extends Buffer<int> {
+  @override
+  int get width => 0;
+
+  @override
+  int get height => 1;
+
+  @override
+  noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
+}
+
+final class _ZeroHeightBuffer extends Buffer<int> {
+  @override
+  int get width => 1;
+
+  @override
+  int get height => 0;
+
+  @override
+  noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
+}
